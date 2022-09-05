1d815e29b3ebba788111e72d5f27e2c77a094e72
na1307 <na13072017@hotmail.com>
2022-09-05 14:54:53 +0900

    Use RegexType Enum

--- removeUC.py
+++ removeUC.py
@@ -3,27 +3,30 @@ from typing import Pattern
 from sys import stderr
 import pywikibot
 from pywikibot import pagegenerators
+import enum
 
-def getRegex(tl: str) -> Pattern:
-    if tl not in ('del', 'uc'): raise Exception('Nope.')
+class RegexType(enum.Enum):
+    Delete = enum.auto()
+    Uncategorized = enum.auto()
 
+def getRegex(tl: RegexType) -> Pattern:
     #UCTLS stands for Uncategorized Templates
     UCTLS = "\{\{(분류( ?필요| 없음)|uncategorized)\}\}\n?"
 
     #DELTLS stands for Delete Templates
     DELTLS = "\{\{(삭제 ?(신|요)청|삭신?|ㅅ{1,2}|ㅆ|del(ete)?|speedy(delete)?)\|"
 
-    return re.compile({'del': DELTLS, 'uc': UCTLS}.get(tl), re.IGNORECASE)
+    return re.compile({RegexType.Delete: DELTLS, RegexType.Uncategorized: UCTLS}.get(tl), re.IGNORECASE)
 
 def main():
     for page in pagegenerators.CategorizedPageGenerator(pywikibot.Category(pywikibot.Site(), '분류:분류 필요 문서')):
         text_base = page.text
 
-        if not getRegex('del').search(text_base):
+        if not getRegex(RegexType.Delete).search(text_base):
             print('"' + page.title() + '" is not a candidate for speedy deletion. skipping...')
             continue
 
-        text_mod = getRegex('uc').sub('', text_base)
+        text_mod = getRegex(RegexType.Uncategorized).sub('', text_base)
 
         print('Page : "' + page.title() + '"', file=stderr)
         pywikibot.showDiff(text_base, text_mod)
